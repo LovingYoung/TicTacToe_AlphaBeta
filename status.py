ad5352768c20d1163862574ae2bcb192d4f72c0b
LovingYoung <782454532@qq.com>
2015-11-15 23:23:29 +0800

    fix some bug

--- status.py
+++ status.py
@@ -19,6 +19,12 @@ class status:
         minPlayer = 0
         maxPlayer = 0
 
+        (win, who) = self.isCompleteAndMessage()
+        if win == True and who[:3] == 'MAX':
+            return -100 + self._level
+        if win == True and who[:3] == 'MIN':
+            return 100 - self._level
+
         pos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
         for i in pos:
