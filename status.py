dee255597424a1c97de20b0737249000ef9b2cc3
LovingYoung <ly950710@gmail.com>
2015-11-23 14:36:41 +0800

    fix some bug

--- status.py
+++ status.py
@@ -24,9 +24,11 @@ class status:
 
         (win, who) = self.isCompleteAndMessage()
         if win == True and who == 1:
-            return -100 + self._level
+            self._priority = -100 + self._level
+            return self._priority
         if win == True and who == 2:
-            return 100 - self._level
+            self._priority = 100 - self._level
+            return self._priority
 
         pos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
